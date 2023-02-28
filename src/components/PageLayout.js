78ac560e188a4d6a3f1d1104a6b03ef433ebe53d
paulblaze <paulpandi@blaze.ws>
2023-02-28 18:23:56 +0530

    feat: Bet NFT package

---
+++ src/components/PageLayout.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import VuiBox from 'vui-theme/components/VuiBox';
+
+export default function PageLayout({ children }) {
+  return (
+    <VuiBox
+      width="100vw"
+      maxWidth="100%"
+      height="100%"
+      minHeight="100vh"
+      sx={({ functions: { tripleLinearGradient }, palette: { gradients } }) => ({
+        overflowX: 'hidden',
+        // backgroundImage: tripleLinearGradient(
+        //   gradients.cover.main,
+        //   gradients.cover.state,
+        //   gradients.cover.stateSecondary,
+        //   gradients.cover.deg
+        // ),
+        backgroundColor : 'mediumblue',
+        position: 'relative',
+      })}
+    >
+      {children}
+    </VuiBox>
+  );
+}
+
+PageLayout.defaultProps = {
+  background: 'default',
+};
+
+PageLayout.propTypes = {
+  background: PropTypes.oneOf(['white', 'light', 'default']),
+  children: PropTypes.node.isRequired,
+};
